d8e4a86ddc148534ef55674f1cc78cb64b950565
Zephy <milotaku07@gmail.com>
2021-01-07 00:36:20 -0500

    V.0.0.1- Primer despliegue del portafolio: Home,NotFound,loader

---
+++ src/app/@templates/loader/loader.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+
+export class LoaderComponent implements OnInit {
+  show: boolean = false;
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(e=>{
+      if(e instanceof NavigationStart){
+        this.show = true;
+      }else if (e instanceof NavigationEnd){
+        this.show = false;
+      }
+    })
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule, routesComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './@templates/loader/loader.component';
+import { NotFoundComponent } from './@templates/not-found/not-found.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    routesComponents,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+    
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
